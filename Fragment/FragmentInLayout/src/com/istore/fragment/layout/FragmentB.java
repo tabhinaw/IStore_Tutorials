c609cac90d5809774f62f42cdc24b477ced9fc33
Abhinav <ashat.mishra@gmail.com>
2015-04-08 00:11:09 +0530

    [Abhinaw] - Added Fragment example through xml.

---
+++ Fragment/FragmentInLayout/src/com/istore/fragment/layout/FragmentB.java
@@ -0,0 +1,15 @@
+package com.istore.fragment.layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentB extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    	View rootView = inflater.inflate(R.layout.fragment_b, container, false);
+        return rootView;
+    }
+}
